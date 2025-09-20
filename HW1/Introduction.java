91933df15ed29e4f92fb5f7019729ad0a384520b
LeNgocPhong <lephong.090106@gmail.com>
2025-09-20 14:12:14 +0700

    add Introduction.java

---
+++ HW1/Introduction.java
@@ -0,0 +1,15 @@
+public class Intruduction {
+    public static void main(String[] args) {
+        System.out.println("Lê Ngọc Phong"
+                + '\t' + "24021593"
+                + '\t' + "CS5"
+                + '\t' + "LeNgocPhong"
+                + '\t' + "24031593@vnu.edu.vn"
+        );
+        for (int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+            System.out.println("No more bottles of beer on the wall.");
+        }
+    }
+}
