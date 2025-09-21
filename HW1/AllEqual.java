cfd2ee7bb5ded37a7b7db808a0bf2ace0414be5a
LeNgocPhong <lephong.090106@gmail.com>
2025-09-21 08:09:46 +0700

    add AllEqual.java

---
+++ HW1/AllEqual.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AllEqual {
+    /**
+     * Prints "equal" if a = b = c and "not equal" otherwise.
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a == b && b == c) {
+            System.out.println("equal");
+        }
+        else {
+            System.out.println("not equal");
+        }
+    }
+}
